e9e5b0570943f22034d5da90c0000fbe011b9c2f
brianwaithaka <waithakabrian8@gmail.com>
2026-03-16 20:32:45 +0300

    create privateroute

---
+++ client/src/components/PrivateRoute.jsx
@@ -0,0 +1,25 @@
+// src/components/PrivateRoute.jsx
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <div className="loading-screen">
+        <div className="spinner"></div>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
+
+export default PrivateRoute;
