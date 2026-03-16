cc8ce4c32981923e6976bbc0692400f69394f4a2
brianwaithaka <waithakabrian8@gmail.com>
2026-03-16 20:44:54 +0300

    add aliases

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-// src/App.jsx
+// src/App.jsx - Updated with aliases
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -6,21 +6,21 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import { AuthProvider } from "./contexts/AuthContext";
-import { ThemeProvider } from "./contexts/ThemeContext";
-import PrivateRoute from "./components/PrivateRoute";
-import Layout from "./components/Layout";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import Properties from "./pages/Properties";
-import PropertyDetail from "./pages/PropertyDetail";
-import Tenants from "./pages/Tenants";
-import TenantDetail from "./pages/TenantDetail";
-import RentPayments from "./pages/RentPayments";
-import PaymentDetail from "./pages/PaymentDetail";
-import Settings from "./pages/Settings";
-import "./App.css";
+import { AuthProvider } from "@contexts/AuthContext";
+import { ThemeProvider } from "@contexts/ThemeContext";
+import PrivateRoute from "@components/PrivateRoute";
+import Layout from "@components/Layout";
+import Login from "@pages/Login";
+import Register from "@pages/Register";
+import Dashboard from "@pages/Dashboard";
+import Properties from "@pages/Properties";
+import PropertyDetail from "@pages/PropertyDetail";
+import Tenants from "@pages/Tenants";
+import TenantDetail from "@pages/TenantDetail";
+import RentPayments from "@pages/RentPayments";
+import PaymentDetail from "@pages/PaymentDetail";
+import Settings from "@pages/Settings";
+import "@styles/App.css";
 
 function App() {
   return (
